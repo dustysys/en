5124d63adb3a2548725f41a5c93d8ddb5b876d87
dustysys <dustysys@protonmail.com>
2017-07-23 07:20:17 -0400

    add automation and integration

---
+++ test/src/sample.spec.js
@@ -0,0 +1,44 @@
+
+
+// Mocha, Chai test
+
+describe('karma test with Chai', function() {
+  it('should expose the Chai assert method', function() {
+    assert.ok('everything', 'everything is ok');
+  })
+
+  it('should expose the Chai expect method', function() {
+    expect('foo').to.not.equal('bar');
+  })
+
+  it('should expose the Chai should property', function() {
+    should.exist(123);
+    (1).should.not.equal(2);
+  })
+
+})
+
+
+
+// Sinon test
+
+describe('Testing the throttle function', function() {
+  var clock
+
+  before(function() { clock = sinon.useFakeTimers() })
+  after(function() { clock.restore() })
+
+  it('calls callback after 100ms', function() {
+    const callback = sinon.spy()
+
+    throttle(callback)()
+
+    clock.tick(99)
+    expect(callback.notCalled).to.be.true
+
+    clock.tick(1)
+    expect(callback.calledOnce).to.be.true
+
+    expect(new Date().getTime()).to.equal(100)
+  })
+});
