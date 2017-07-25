52c2bd702a18a1eba9a73505e4c7d2812b0e819c
dustysys <dustysys@protonmail.com>
2017-07-25 14:40:34 -0400

    try selenium+travis

--- test/src/selen_test.spec.js
+++ test/src/selen_test.spec.js
@@ -3,17 +3,26 @@
 var assert = require('assert'),
 test = require('selenium-webdriver/testing'),
 webdriver = require('selenium-webdriver');
+chrome = require('selenium-webdriver/chrome');
  
 test.describe('Google Search', function() {
   this.timeout(30000);
   test.it('should work', function() {
+    var options = new chrome.Options();
+    options.setChromeBinaryPath('/usr/bin/google-chrome-stable');
+    options.addArguments('--headless');
+    options.addArguments('--disable-gpu');
+    
     var driver = new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.chrome()).
-    build();
+         forBrowser('chrome').
+         setChromeOptions(options).
+         build();
 driver.get('http://www.google.com');
     var searchBox = driver.findElement(webdriver.By.name('q'));
+    console.log("Hello");
     searchBox.sendKeys('simple programmer');
-    searchBox.getAttribute('value').then(function(value) {
+    searchBox.getAttribute('innerHTML').then(function(value) {
+      console.log("Hello2");
       assert.equal(value, 'simple programmer');
     });
     driver.quit();
