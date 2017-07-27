1c8b0d38cdfb0c949bbe1a7c9f0bd80afd7c89d5
dustysys <dustysys@protonmail.com>
2017-07-27 00:59:49 -0400

    separate event handlers from background script

--- src/js/background.js
+++ src/js/background.js
@@ -3,7 +3,7 @@ File: background.js
 
 This runs in the background and executes functions based on events such
 as alarms, web requests by the user and notifications. It also is the first
-script to run, so it will do initialization if necessary.
+script to run on install, so it will do initialization if necessary.
 #############################################################################*/
 
 var global_alarm_timestamp = Date.now();
@@ -11,88 +11,7 @@ var global_pref_release_update = { enabled: true, interval: 15 };
 var global_pref_list_sync = { enabled: true, interval: 60 };
 var global_pref_notifications = { enabled: true };
 
-/**
- * Syncs user's data. It also checks if the session is valid and
- * attempts a new session if it isn't.
- */
-function bgSync() {
-	sessionIsValid(function (valid_session) {
-		if (valid_session) {
-			pullAllData();
-			updateBadge();
-		}
-		else {
-			attemptNewSession(
-				function success() {
-					console.log("New session initialized from background script");
-					updateBadge();
-				}, function error(msg) {
-					console.log("New session failed to initialize from background script");
-					console.warn("Warning: " + msg);
-				}
-			);
-		}
-	});
-}
 
-/**
- * Updates lists with new releases
- */
-function bgUpdateReleases() {
-	updateLists();
-}
-
-/**
- * check an alarm event for its purpose and execute based on it
- * @param {Event} alarm
- */
-function checkAlarm(alarm) {
-	if (alarm && alarm.name) {
-		var alarm_desc = alarm.name.substring(0, alarm.name.indexOf(":"));
-		var alarm_id = alarm.name.substring(alarm.name.indexOf(":") + 1);
-		if (alarm_id === global_alarm_timestamp.toString()) {
-			if (alarm_desc === "update_all") {
-				console.log("Syncing");
-				bgSync();
-			} else if (alarm_desc === "update_releases") {
-				console.log("Updating releases");
-				bgUpdateReleases();
-			}
-		}
-		else {
-			console.log("Deleting old alarm: " + alarm.name);
-			chrome.alarms.clear(alarm.name);
-		}
-	}
-}
-
-/**
- * check message passed from another script and execute based on it
- * @param {Object} message
- * @param {Object} sender
- * @param {function(Object)} sendResponse
- */
-function checkMessage(message, sender, sendResponse) {
-	if (message) {
-		if (message.hasOwnProperty("src") && message.src === "en_popup") {
-			if (message.title === "UPDATED_PREFERENCE") {
-				bgUpdatePrefs();
-				var response = {
-					src: "en_bg",
-					title: "ACK_UPDATED_PREFERENCE"
-				};
-				sendResponse(response);
-			} else if (message.title === "REQ_UPDATE_BADGE") {
-				updateBadge();
-				var response = {
-					src: "en_bg",
-					title: "ACK_UPDATE_BADGE"
-				};
-				sendResponse(response);
-			}
-		}
-	}
-}
 
 // TODO: send popup messages about new updates so it can immediately show them to user
 function sendMessage() {
@@ -116,14 +35,6 @@ function scheduleSyncs() {
 		var alarm_name = "update_all:" + global_alarm_timestamp;
 		chrome.alarms.create(alarm_name, { periodInMinutes: global_pref_list_sync.interval });
 	}
-}
-
-/**
- * opens the link associated with a notification in a new tab
- * @param {string} notif_id
- */
-function openNotificationLink(notif_id) {
-	chrome.tabs.create({ active: true, url: notif_id });
 }
 
 // listens for notification click events 
