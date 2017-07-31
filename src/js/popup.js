fa9e1cbff3ed66bfcdea506f9f8204d1ed0ebaa5
dustysys <dustysys@protonmail.com>
2017-07-31 06:53:11 -0400

    use strict for popup runtime

--- src/js/popup.js
+++ src/js/popup.js
@@ -5,4 +5,5 @@ This is the entry point chrome loads when opening the popup.
 #############################################################################*/
 
 // startup popup once DOM loads
+"use strict";
 document.addEventListener('DOMContentLoaded', popupInit);
